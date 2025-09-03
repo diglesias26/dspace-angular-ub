1ee29319f7f6dbf9743b46f2b0061fa463a60e30
hernandez <hernandez@ub.edu>
2025-09-03 18:25:31 +0200

    menu dins de header

--- src/themes/ub/app/header/header.component.ts
+++ src/themes/ub/app/header/header.component.ts
@@ -6,8 +6,8 @@ import { HeaderComponent as BaseComponent } from '../../../../app/header/header.
  */
 @Component({
   selector: 'ds-header',
-  styleUrls: ['header.component.scss'],
-  templateUrl: 'header.component.html',
+  styleUrls: ['./header.component.scss'],
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent extends BaseComponent {
 }
