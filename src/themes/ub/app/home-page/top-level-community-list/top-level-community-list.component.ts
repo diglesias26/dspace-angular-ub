d515636a120b4b0e41baf52ae76b5635f43f455a
Oriol <oooriii@gmail.com>
2025-09-29 13:03:49 +0200

    community home - css

--- src/themes/ub/app/home-page/top-level-community-list/top-level-community-list.component.ts
+++ src/themes/ub/app/home-page/top-level-community-list/top-level-community-list.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { TopLevelCommunityListComponent as BaseComponent } from '../../../../../app/home-page/top-level-community-list/top-level-community-list.component';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'ds-top-level-community-list',
-  // styleUrls: ['./top-level-community-list.component.scss'],
-  styleUrls: ['../../../../../app/home-page/top-level-community-list/top-level-community-list.component.scss'],
-  // templateUrl: './top-level-community-list.component.html'
-  templateUrl: '../../../../../app/home-page/top-level-community-list/top-level-community-list.component.html'
+  styleUrls: ['./top-level-community-list.component.scss'],
+  // styleUrls: ['../../../../../app/home-page/top-level-community-list/top-level-community-list.component.scss'],
+  templateUrl: './top-level-community-list.component.html'
+  // templateUrl: '../../../../../app/home-page/top-level-community-list/top-level-community-list.component.html'
 })
 
-export class TopLevelCommunityListComponent extends BaseComponent {}
+export class TopLevelCommunityListComponent extends BaseComponent {
+/** get info from the community */
+getCommunityTitle(community: any) {
+  let title = community.metadata['dc.title'][0];
+  // if no title, return an empty string
+  if (!title) {
+    return '';
+  }
+  return title.value;
+}
+
+getCommunityLink(community: any) {
+  let link = community['_links']['self'];
+  // if no title, return an empty string
+  if (!link) {
+    return '';
+  }
+  return link.href;
+}
+
+
+
+}
 
