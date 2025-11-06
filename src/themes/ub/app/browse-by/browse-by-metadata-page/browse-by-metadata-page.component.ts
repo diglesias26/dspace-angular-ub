7b96ea10275583ed4ceefa8b38dfa46801daa426
hernandez <hernandez@ub.edu>
2025-11-06 10:29:02 +0100

    link al thesaurus ub

--- src/themes/ub/app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.ts
+++ src/themes/ub/app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.ts
@@ -3,10 +3,10 @@ import { BrowseByMetadataPageComponent as BaseComponent } from '../../../../../a
 
 @Component({
   selector: 'ds-browse-by-metadata-page',
-  // styleUrls: ['./browse-by-metadata-page.component.scss'],
-  styleUrls: ['../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.scss'],
-  // templateUrl: './browse-by-metadata-page.component.html'
-  templateUrl: '../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.html'
+  styleUrls: ['./browse-by-metadata-page.component.scss'],
+  // styleUrls: ['../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.scss'],
+  templateUrl: './browse-by-metadata-page.component.html'
+  // templateUrl: '../../../../../app/browse-by/browse-by-metadata-page/browse-by-metadata-page.component.html'
 })
 
 /**
