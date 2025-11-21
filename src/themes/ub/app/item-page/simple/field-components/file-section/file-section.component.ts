f1e1f2e51f5dd41ca3c85991fe58d8bfcdbb84ab
hernandez <hernandez@ub.edu>
2025-11-21 12:59:39 +0100

    modifico file section a simple item page

--- src/themes/ub/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/themes/ub/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -4,8 +4,8 @@ import { FileSectionComponent as BaseComponent } from '../../../../../../../app/
 
 @Component({
     selector: 'ds-item-page-file-section',
-    // templateUrl: './file-section.component.html',
-    templateUrl: '../../../../../../../app/item-page/simple/field-components/file-section/file-section.component.html',
+    templateUrl: './file-section.component.html',
+    // templateUrl: '../../../../../../../app/item-page/simple/field-components/file-section/file-section.component.html',
     animations: [slideSidebarPadding],
 })
 export class FileSectionComponent extends BaseComponent {
