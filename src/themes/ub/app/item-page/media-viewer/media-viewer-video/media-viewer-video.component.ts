dcb5072ec433a118acbd686eb10b3a71fce93059
hernandez <hernandez@ub.edu>
2026-04-13 20:48:42 +0200

    video viewer fix

--- src/themes/ub/app/item-page/media-viewer/media-viewer-video/media-viewer-video.component.ts
+++ src/themes/ub/app/item-page/media-viewer/media-viewer-video/media-viewer-video.component.ts
@@ -7,8 +7,8 @@ import {
   selector: 'ds-media-viewer-video',
   templateUrl: './media-viewer-video.component.html',
   // templateUrl: '../../../../../../app/item-page/media-viewer/media-viewer-video/media-viewer-video.component.html',
-  // styleUrls: ['./media-viewer-video.component.scss'],
-  styleUrls: ['../../../../../../app/item-page/media-viewer/media-viewer-video/media-viewer-video.component.scss'],
+  styleUrls: ['./media-viewer-video.component.scss'],
+  // styleUrls: ['../../../../../../app/item-page/media-viewer/media-viewer-video/media-viewer-video.component.scss'],
 })
 export class MediaViewerVideoComponent extends BaseComponent {
 }
